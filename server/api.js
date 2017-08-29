df2c99bcdd53a7ef9c3fd88c910af9e1cccd0b04
jn9cn <jannine.chan@gmail.com>
2017-08-29 15:22:56 -0400

    models and routes

--- server/api.js
+++ server/api.js
@@ -1,10 +1,24 @@
 'use strict'
-const api = require('express').Router()
-const db = require('../db')
+const router = require('express').Router()
+const { Campus, Student } = require('../db/models');
 
 // If you aren't getting to this object, but rather the index.html (something with a joke) your path is wrong.
 	// I know this because we automatically send index.html for all requests that don't make sense in our backend.
 	// Ideally you would have something to handle this, so if you have time try that out!
-api.get('/hello', (req, res) => res.send({hello: 'world'}))
+// api.get('/', (req, res) => res.send({hello: 'world'}))
 
-module.exports = api
+module.exports = router;
+
+router.use('/campuses', require('./campuses'));
+router.use('/students', require('./students'));
+
+router.use((req, res, next) => {
+	res.status(404).send('Not found');
+});
+
+
+// api.use((err, req, res, next) => {
+// 	console.error(err);
+// 	res.sendStatus(500).send(err.message);
+// });
+
